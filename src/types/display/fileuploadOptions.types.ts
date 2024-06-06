b4045591e6d6d854833a6d448cdc8d6b3785e101
Robin Reyer <robin.reyer@t-online.de>
2024-06-06 15:40:33 +0200

    DOC-8 Dokumentation der displayOptions und fileUploadOptions über JSDOC-Kommentare

--- src/types/display/fileuploadOptions.types.ts
+++ src/types/display/fileuploadOptions.types.ts
@@ -1,4 +1,15 @@
+/**
+ * @description File upload options. Used to define the allowed file extensions and maximum file size for a file upload field
+ */
 export type TFileUploadOptions = {
+	/**
+	 * @description Maximum file size in bytes
+	 * @example 1000000
+	 */
 	maxFileSize?: number;
+	/**
+	 * @description Allowed file extensions
+	 * @example ".jpg, .png"
+	 */
 	allowedFileExtensions?: string;
 };
